2e14afd37d31ca83e4047b3961203aab7734b45e
virender <sidhu2772000@gmail.com>
2021-07-24 13:17:46 +0530

    add sass and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './components/Board';
 
+import './styles/root.scss';
+
 export default () => (
-  <>
+  <div className="app">
     <h1>Zero-Kanta</h1>
     <p>Created by Virender Singh</p>
     <Board />
-  </>
+  </div>
 );
