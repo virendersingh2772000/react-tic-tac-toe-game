56d113a649fa646eeaf735e503586c973c9e3c9b
virender <sidhu2772000@gmail.com>
2021-07-24 16:04:38 +0530

    added board state and basic logics

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNextX, setisNextX] = useState(false);
+
+  const handleClick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos == position) {
+          return isNextX ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setisNextX(prev => !prev);
+  };
+
+  const renderposition = position => {
+    return (
+      <Square value={board[position]} onClick={() => handleClick(position)} />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderposition(0)}
+        {renderposition(1)}
+        {renderposition(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderposition(3)}
+        {renderposition(4)}
+        {renderposition(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderposition(6)}
+        {renderposition(7)}
+        {renderposition(8)}
       </div>
     </div>
   );
